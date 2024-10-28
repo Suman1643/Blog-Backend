8a3339b65652f2454771c6cd8abc26fc474e9e22
Suman Kumar <sumankumar01643@gmail.com>
2024-10-28 19:09:44 +0530

    update

--- src/routes/blogRouter.js
+++ src/routes/blogRouter.js
@@ -5,10 +5,10 @@ import CustomError from '../middlewares/customerror.js';
 
 const router = express.Router();
 
-router.post('/post', isAuntheticated, isAuthorized("Author"), blogPost);
+router.post('/post', isAuthorized("Author"), blogPost);
 router.delete('/delete/:id', isAuntheticated, isAuthorized("Author"), deleteBlog);
 router.get('/all', getallBlogs);
-router.get('/singleblog/:id', isAuntheticated, getSingleBlog);
+router.get('/singleblog/:id', getSingleBlog);
 router.get('/myblog', isAuntheticated, isAuthorized("Author"), getMyBlog);
 
 
